0a50babf42eafc74962876a3ef9c0becdde7e28f
Sathish Kumar Vadivel <sathishkumarirtt@yahoo.co.in>
2026-09-24 16:46:11 +0200

    falcon mode as per the uboot documentation

--- board/beagleboard/beaglebone/post-build.sh
+++ board/beagleboard/beaglebone/post-build.sh
@@ -6,4 +6,7 @@ BOARD_DIR="$(dirname "$0")"
 #cp "${BOARD_DIR}/uEnv.txt" "${BINARIES_DIR}/uEnv.txt"
 
 #install -m 0644 -D "${BOARD_DIR}/extlinux.conf" "${BINARIES_DIR}/extlinux/extlinux.conf"
-cp "${BOARD_DIR}/args" "${BINARIES_DIR}/args"
+
+# Falcon mode expects a device tree blob at the raw 'args' sector, not plain text
+cp "${BINARIES_DIR}/am335x-boneblack.dtb" "${BINARIES_DIR}/args"
+"${HOST_DIR}/bin/fdtput" -t s "${BINARIES_DIR}/args" /chosen bootargs "$(cat "${BOARD_DIR}/args")"
